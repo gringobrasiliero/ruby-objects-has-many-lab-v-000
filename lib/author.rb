fb7aa44a3581aa908cbbdb6b853bd2b48dc5e9b5
Nolan Byrnes <nolanehs@gmail.com>
2017-11-19 14:23:13 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -25,12 +25,6 @@ post_name.author=self
   @@post_count+=1
 end
 
-def add_post_by_title(title)
-    new_post = Post.new(title)
-    self.posts << new_post
-    new_post.author = self
-    @@post_count+=1
-  end
 
 
 def self.post_count
